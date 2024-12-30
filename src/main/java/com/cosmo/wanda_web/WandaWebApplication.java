9c3a3c7dcf0036440e1a5bb65047e417fdc2f459
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2024-12-30 14:31:11 -0300

    initial config

---
+++ src/main/java/com/cosmo/wanda_web/WandaWebApplication.java
@@ -0,0 +1,13 @@
+package com.cosmo.wanda_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WandaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WandaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmo/wanda_web/WandaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmo.wanda_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WandaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
