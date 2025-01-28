10c4175afa90fd2951a212961aee09aeb367a834
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2025-01-28 11:28:16 -0300

    remove matchPk and add Id in Match : dessa forma, conseguimos criar varios matchs

--- src/main/java/com/cosmo/wanda_web/entities/Match.java
+++ src/main/java/com/cosmo/wanda_web/entities/Match.java
@@ -1,47 +1,65 @@
 package com.cosmo.wanda_web.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_match")
 public class Match {
 
-    @EmbeddedId
-    private MatchPK id = new MatchPK();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Adiciona um identificador único
+
+    @ManyToOne
+    @JoinColumn(name = "player1_id")
+    private User player1;
+
+    @ManyToOne
+    @JoinColumn(name = "player2_id")
+    private User player2;
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private Long winner;
+
+    @ManyToOne
+    @JoinColumn(name = "winner_id")
+    private User winner;
 
     public Match() {
     }
 
-    public Match(User player1, User player2,  Instant moment, Long winner) {
-        id.setPlayer1(player1);
-        id.setPlayer2(player2);
+    public Match(User player1, User player2, Instant moment, User winner) {
+        this.player1 = player1;
+        this.player2 = player2;
         this.moment = moment;
         this.winner = winner;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public User getPlayer1() {
-        return id.getPlayer1();
+        return player1;
     }
 
     public void setPlayer1(User player1) {
-        id.setPlayer1(player1);
+        this.player1 = player1;
     }
 
     public User getPlayer2() {
-        return id.getPlayer2();
+        return player2;
     }
 
     public void setPlayer2(User player2) {
-        id.setPlayer2(player2);
+        this.player2 = player2;
     }
 
     public Instant getMoment() {
@@ -52,11 +70,29 @@ public class Match {
         this.moment = moment;
     }
 
-    public Long getWinner() {
+    public User getWinner() {
         return winner;
     }
 
-    public void setWinner(Long winner) {
+    public void setWinner(User winner) {
         this.winner = winner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Match match = (Match) o;
+
+        if (!Objects.equals(id, match.id)) return false;
+        return Objects.equals(moment, match.moment);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (moment != null ? moment.hashCode() : 0);
+        return result;
     }
 }
