2a050287de4d020aa7f209caab7a4e0580887cec
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2026-03-14 13:39:17 -0300

    feat: add GameEngine, FunctionInfo e MatchResult (dtos)

--- src/main/java/com/cosmo/wanda_web/infra/GameEngine.java
+++ src/main/java/com/cosmo/wanda_web/infra/GameEngine.java
@@ -1,9 +1,17 @@
 package com.cosmo.wanda_web.infra;
 
-import com.cosmo.wanda_web.entities.Challenge;
-import com.cosmo.wanda_web.entities.Game;
+import com.cosmo.wanda_web.entities.User;
+import com.cosmo.wanda_web.infra.dtos.FunctionInfo;
+import com.cosmo.wanda_web.infra.dtos.MatchResult;
+import com.cosmo.wanda_web.repositories.FunctionRepository;
+
+import java.util.List;
 
 public interface GameEngine {
     String gameName(); // retorna o nome
-    Long run(Long playerId1, Long playerId2); // Roda a engine e retorna o ID do Match (ou lança exceção)
+    List<FunctionInfo> functions();
+    boolean hasAllFunctions(Long userId, FunctionRepository repository);
+    MatchResult run(User player1, User player2);
+    Object parseReplay(String replayJson);
+
 }
